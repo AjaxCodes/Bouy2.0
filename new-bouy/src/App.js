b85b1bb84413882a43f12d86907daf359e95b2c0
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-08 22:51:40 -0500

    Header.js/css created in components folder. Imagesfolder added to project, includes logo,4 shirt designs for storfront

--- new-bouy/src/App.js
+++ new-bouy/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>BOUY</h1>
-    </div>
-  )
+  return <div className="app">
+    <Header/>
+  </div>;
 }
 
-export default App
+export default App;
