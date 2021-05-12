1e3c1410fef01223011bfba9ffd835c402f5f083
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-12 15:51:59 -0500

    seeding data for Resources.js civilian / vetrean section. links added to civilian.js, still working out href issue

--- new-bouy/src/Components/Civilian.js
+++ new-bouy/src/Components/Civilian.js
@@ -12,22 +12,44 @@ function Civilian() {
       </h2>
 
       <div className="civilian__resources">
-        <Resource
+      <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
           title="LIFELINE"
           summary="We can all help prevent suicide. The Lifeline provides 24/7,
            free and confidential support for people in distress, prevention and 
            crisis resources for you or your loved ones, and best practices for professionals."
-          listing=" steve .com"
-          url="https://suicidepreventionlifeline.org"
+          listing="https://suicidepreventionlifeline.org"
         />
         <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing=" steve .com"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo8xt2KoXvdkS92tnJ6TS7H0TKda-LcrpAbg&usqp=CAU"
+          title="AFPS"
+          summary="
+          You're not alone
+          Whether you have struggled with suicide yourself or have lost a loved one, know you are not alone. Hear about
+           personal experiences from people in your local community whose lives have been impacted by suicide."
+          listing="https://afsp.org"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTklG_9nVnC0RXdNo6MqK4TJce41y4OT8UfIA&usqp=CAU"
+          title="SPRC"
+          summary="We all have a role to play. Together, we can save lives."
+          listing="https://www.sprc.org"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVwNCz6QfQgET45bo_2jfQjYj8txDqH5DVzQ&usqp=CAU"
+          title="NIMH"
+          summary="Call the National Suicide Prevention Lifeline, or text the Crisis Both services are free and available 
+          24 hours a day, seven days a week. The deaf and hard of hearing cancontact the Lifeline via TTY at 1-800-799-4889.
+          All calls are confidential. "
+          listing="https://www.nimh.nih.gov/health/topics/suicide-prevention/"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUb4Cu8X7Ih7cvLrO_KG_MuYoYIGQxmxa9PQ&usqp=CAU"
+          title="crisis Line Text"
+          summary="Text HOME to 741741 from anywhere in the United States, anytime. Crisis Text Line is here for any crisis.
+           A live, trained Crisis Counselor receives the text and responds, all from our secure online platform. The volunteer
+            Crisis Counselor will help you move from a hot moment to a cool moment."
+          listing="https://www.crisistextline.org"
         />
         <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
@@ -53,55 +75,7 @@ function Civilian() {
            crisis resources for you or your loved ones, and best practices for professionals."
           listing="https://suicidepreventionlifeline.org"
         />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
+         <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
           title="LIFELINE"
           summary="We can all help prevent suicide. The Lifeline provides 24/7,
--- new-bouy/src/Components/Resources.js
+++ new-bouy/src/Components/Resources.js
@@ -55,12 +55,35 @@ function Resources() {
           listing="https://suicidepreventionlifeline.org"
         />
         <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo8xt2KoXvdkS92tnJ6TS7H0TKda-LcrpAbg&usqp=CAU"
+          title="AFPS"
+          summary="
+          You're not alone
+          Whether you have struggled with suicide yourself or have lost a loved one, know you are not alone. Hear about
+           personal experiences from people in your local community whose lives have been impacted by suicide."
+          listing="https://afsp.org"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTklG_9nVnC0RXdNo6MqK4TJce41y4OT8UfIA&usqp=CAU"
+          title="SPRC"
+          summary="We all have a role to play. Together, we can save lives."
+          listing="https://www.sprc.org"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVwNCz6QfQgET45bo_2jfQjYj8txDqH5DVzQ&usqp=CAU"
+          title="NIMH"
+          summary="Call the National Suicide Prevention Lifeline, or text the Crisis Both services are free and available 
+          24 hours a day, seven days a week. The deaf and hard of hearing cancontact the Lifeline via TTY at 1-800-799-4889.
+          All calls are confidential. "
+          listing="https://www.nimh.nih.gov/health/topics/suicide-prevention/"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUb4Cu8X7Ih7cvLrO_KG_MuYoYIGQxmxa9PQ&usqp=CAU"
+          title="crisis Line Text"
+          summary="Text HOME to 741741 from anywhere in the United States, anytime. Crisis Text Line is here for any crisis.
+           A live, trained Crisis Counselor receives the text and responds, all from our secure online platform. The volunteer
+            Crisis Counselor will help you move from a hot moment to a cool moment."
+          listing="https://www.crisistextline.org"
         />
         <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
@@ -86,21 +109,29 @@ function Resources() {
            crisis resources for you or your loved ones, and best practices for professionals."
           listing="https://suicidepreventionlifeline.org"
         />
+      
+      
+      </div>
+
+      <h2>
+        <GradeIcon /> <GradeIcon /> <GradeIcon /> Vetreans <GradeIcon />{" "}
+        <GradeIcon /> <GradeIcon />
+      </h2>
+
+      <div className="resources__vet">
         <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVwNCz6QfQgET45bo_2jfQjYj8txDqH5DVzQ&usqp=CAU"
+          title="NIMH"
+          summary="Call the National Suicide Prevention Lifeline, or text the Crisis Both services are free and available 
+          24 hours a day, seven days a week. The deaf and hard of hearing cancontact the Lifeline via TTY at 1-800-799-4889.
+          All calls are confidential. "
+          listing="https://www.nimh.nih.gov/health/topics/suicide-prevention/"
         />
         <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiy48Du7AdT3Fo44_FxJ4CAWp596DNpTgKyQ&usqp=CAU"
+          title="Vetreans Crisis Line"
+          summary="No matter what you are experiencing, there is support for getting your life back on track"
+          listing="https://www.veteranscrisisline.net"
         />
         <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
@@ -137,140 +168,11 @@ function Resources() {
       </div>
 
       <h2>
-        <GradeIcon /> <GradeIcon /> <GradeIcon /> Vetreans <GradeIcon />{" "}
-        <GradeIcon /> <GradeIcon />
+        <SecurityIcon /> <SecurityIcon /> <SecurityIcon /> First Responders
+        <SecurityIcon /> <SecurityIcon /> <SecurityIcon />
       </h2>
 
-      <div className="resources__vet">
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-      </div>
-      
-        <h2>
-          <SecurityIcon /> <SecurityIcon /> <SecurityIcon /> First Responders
-          <SecurityIcon /> <SecurityIcon /> <SecurityIcon />
-        </h2>
-      
-
       <div className="resources">
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
-        <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="LIFELINE"
-          summary="We can all help prevent suicide. The Lifeline provides 24/7,
-           free and confidential support for people in distress, prevention and 
-           crisis resources for you or your loved ones, and best practices for professionals."
-          listing="https://suicidepreventionlifeline.org"
-        />
         <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
           title="LIFELINE"
