216105a27dca11f687d1678160605808838a11d1
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-16 17:44:27 -0500

    Resources.js seeding first responder data

--- new-bouy/src/Components/Resources.js
+++ new-bouy/src/Components/Resources.js
@@ -148,6 +148,11 @@ function Resources() {
           and non-Veteran patients. Contact us today."
           listing="https://www.ptsd.va.gov"
         />
+        <Resource 
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMN4f1N6WVa8Kgg4YUUdkW8cx2_XCdRUoC6w&usqp=CAU"
+         title="The Columbia Lighthouse Project" 
+         summary="Suicide Prevention for First Responders Resources”" 
+         listing="https://firstwatch.net/suicide-prevention-for-first-responders-resources/" />
         <Resource
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8N2V5hBxje1_lQ3ZSp6uyXWTJzWnG09LGMQ&usqp=CAU"
           title="PREVENT 22"
@@ -165,8 +170,6 @@ function Resources() {
           and non-Veteran patients. Contact us today."
           listing="https://www.mentalhealth.va.gov/suicide_prevention/"
         />
-       
-       
       </div>
 
       <h2>
@@ -182,44 +185,48 @@ function Resources() {
            free and confidential support for people in distress, prevention and 
            crisis resources for you or your loved ones, and best practices for professionals."
           listing="https://suicidepreventionlifeline.org"
-        /> 
+        />
         <Resource
-        image=""
-        title=""
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTklG_9nVnC0RXdNo6MqK4TJce41y4OT8UfIA&usqp=CAU"
+          title="SPRC"
+          summary="We all have a role to play. Together, we can save lives."
+          listing="https://www.sprc.org"
+        />
+        <Resource
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPB19-Emzor6AGCutwVMPUg2eLPIokYmEjg&usqp=CAU"
+          title="The Green Code Campaign"
+          summary="The Code Green Campaign® is a first responder oriented mental health advocacy and education
+           organization. Also known as Code Green, we serve all types of first responders. This includes firefighters,
+            EMTs, paramedics, dispatchers, police, corrections, air medical, and search & rescue. Our name is a combination
+             of the color for mental health awareness (green) and the “code alerts” used in emergency services. If someone
+              is having a stroke or heart attack first responders will call a “code stroke” or “code STEMI”. The idea is that
+               Code Green is calling a code alert on the mental health of first responders."
+          listing="https://codegreencampaign.org"
+        />
+        <Resource 
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMN4f1N6WVa8Kgg4YUUdkW8cx2_XCdRUoC6w&usqp=CAU"
+         title="The Columbia Lighthouse Project" 
+         summary="Suicide Prevention for First Responders Resources”" 
+         listing="https://firstwatch.net/suicide-prevention-for-first-responders-resources/" />
+
+        <Resource 
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZmZvTptXWQ4EAVl8pMziUTay9eGn4Gr-i2A&usqp=CAU" 
+        title="SAMHSA" 
+        summary="Disaster Technical Assistance Center Supplemental Research Bulletin
+        First Responders: Behavioral Health Concerns, Emergency Response, and Trauma" 
+        listing="https://www.samhsa.gov/" />
+
+        <Resource 
+        image="" 
+        title="" 
+        summary="" 
+        listing="" />
+
+        <Resource 
+        image="" 
+        title="" 
         summary=""
-        listing=""
-      />
-       <Resource
-          image=""
-          title=""
-          summary=""
-          listing=""
-        />
-         <Resource
-          image=""
-          title=""
-          summary=""
-          listing=""
-        />
-         <Resource
-          image=""
-          title=""
-          summary=""
-          listing=""
-        />
-         <Resource
-          image=""
-          title=""
-          summary=""
-          listing=""
-        />
-         <Resource
-          image=""
-          title=""
-          summary=""
-          listing=""
-        />
-      
+        listing="" />
       </div>
     </div>
   );
