8609249e173008921420f3d546d49922e4090455
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-15 22:13:26 -0500

    Civilian.js data finished// resources.js 40%

--- new-bouy/src/Components/Resources.js
+++ new-bouy/src/Components/Resources.js
@@ -101,10 +101,14 @@ function Resources() {
           listing="https://www.nami.org"
         />
         <Resource
-          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvK9zDhS6OulLpw0LsllErEaBO3-2Sdv94fQ&usqp=CAU"
-          title="____"
-          summary="____"
-          listing="https://"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThZ5OdPIVZmksd_tWXQcKUYjQa2R3KCbfIxg&usqp=CAU"
+          title="Public Instruction"
+          summary="DPI’s Gatekeeper Training Module
+          An important step in protecting all students from the dangers of suicide is the ability to recognize the warning
+         signs of suicide ideation and take appropriate steps to keep a suicidal student safe. This new, interactive suicide
+         prevention gatekeeper module helps schools and districts train their students and staff. The module takes approximately
+         20 minutes to complete."
+          listing="https://dpi.wi.gov/sspw/mental-health/youth-suicide-prevention"
         />
       </div>
 
