7ca7b3df13100c56af4b9dfd40af168cee72af26
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-19 22:10:35 -0500

    Conversation.js created

---
+++ new-bouy/src/Components/Conversation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Conversation() {
+    return (
+        <div className="conversation">
+            <h1>Conversation</h1>
+        </div>
+    )
+}
+
+export default Conversation
