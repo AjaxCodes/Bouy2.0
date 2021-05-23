711974bbb2122a677f7b66847f30a63dd98f67cd
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-05-23 17:56:05 -0500

    css pages created for orders.js and Basketitem.js

---
+++ new-bouy/src/Components/Basketitem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Basketitem.css";
+
+
+
+function Basketitem() {
+    return (
+        <div className="basketItem">
+            <h1>Basket item</h1>
+        </div>
+    )
+}
+
+export default Basketitem
