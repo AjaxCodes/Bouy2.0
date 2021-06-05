5d3093133502e5ae0cc985f6cbdf26c35508f1fc
Aj Steinbacher <66484594+AjaxCodes@users.noreply.github.com>
2021-06-04 21:53:15 -0500

    subtotal.js algorytm added, working out a dependancy issue with react-currency-format with this version of react

--- new-bouy/src/Components/Cart.js
+++ new-bouy/src/Components/Cart.js
@@ -1,17 +1,16 @@
 import React from "react";
-import "./Cart.css"
+import "./Cart.css";
+import Subtotal from "./Subtotal"
 function Cart() {
   return (
     <div className="cart">
       <div className="cart__left">
         <div>
-          <h3 className="cart__title">
-            Your cart
-          </h3>
+          <h3 className="cart__title">Your cart</h3>
         </div>
       </div>
       <div className="cart__right">
-        <h1>Subtotal</h1>
+        <Subtotal />
       </div>
     </div>
   );
--- new-bouy/src/Components/Subtotal.js
+++ new-bouy/src/Components/Subtotal.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import {  useHistory } from "react-router-dom";
 function Subtotal() {
-  return <div className="subtotal"></div>;
+  const history = useHistory();
+  
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal({0} items)
+              <strong>{value}</strong>
+            </p>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandsSeparator={true}
+        prefix={"$"}
+      />
+      <br/>
+    <button onClick={e => history.push('/payment')}>Pay Now</button>
+    </div>
+  );
 }
+
 
 export default Subtotal;
